c0205e30a2e96d1a948d712b3cba47f1120864c5
EdsonRego <edsonxrego@gmail.com>
2024-02-29 13:21:07 -0300

    Project Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { personasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 //decorador NgModule da classe AppMoodule{}
 @NgModule({
   declarations: [
-    AppComponent, personasComponent
+    AppComponent, personasComponent, PersonaComponent
   ],
   imports: [
     BrowserModule,
