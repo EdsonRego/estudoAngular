490ca6011024c58e56bdf8181a1e620d3cbdaf45
EdsonRego <edsonxrego@gmail.com>
2024-02-29 13:40:28 -0300

    css styles application via en linea

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,7 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-personas',
     templateUrl: './personas.component.html',
-    styleUrls: ['./personas.component.css'] 
+    //styleUrls: ['./personas.component.css'] 
+    styles: [`
+    h1{
+        color: blue;
+    }  
+    `]
 })
 export class personasComponent {
 
